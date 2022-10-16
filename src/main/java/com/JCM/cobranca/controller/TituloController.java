274b8b7ea7144116d0b70e5042fd8c4df1306e52
Julio Cesar Melegari <juliocesar.melegari@gmail.com>
2022-10-16 17:50:10 -0300

    editar, excluir, mensagens na tela, caixa de dialogo

--- src/main/java/com/JCM/cobranca/controller/TituloController.java
+++ src/main/java/com/JCM/cobranca/controller/TituloController.java
@@ -8,9 +8,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.JCM.cobranca.model.StatusTitulo;
 import com.JCM.cobranca.model.Titulo;
@@ -20,27 +22,29 @@ import com.JCM.cobranca.repository.TitulosRepository;
 @RequestMapping("/titulos")
 public class TituloController {
 	
+	private static final String CADASTRO_VIEW = "CadastroTitulo";
+	
 	@Autowired
 	private TitulosRepository titulosRepository;
 	
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
 		mv.addObject(new Titulo());
 		return mv;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView salvar(@Validated Titulo titulo, Errors errors) {
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
+		
 		if (errors.hasErrors()) {
-			return mv;
+			return CADASTRO_VIEW;
 		}
 		
 		titulosRepository.save(titulo);
 		
-		mv.addObject("mensagem", "Título salvo com sucesso!");
-		return mv;
+		attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");
+		return "redirect:/titulos/novo";
 	}
 	
 	@RequestMapping
@@ -50,6 +54,22 @@ public class TituloController {
 		ModelAndView mv = new ModelAndView("PesquisaTitulos");
 		mv.addObject("titulos", ListTitulos);
 		return mv;
+	}
+	
+	@RequestMapping("{codigo}")
+	public ModelAndView edicao(@PathVariable("codigo") Titulo titulo) {
+				
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW); 
+		mv.addObject(titulo);
+		return mv;
+	}
+	
+	@RequestMapping(value="{codigo}", method = RequestMethod.DELETE)
+	public String excluir(@PathVariable Long codigo, RedirectAttributes attributes) {
+		titulosRepository.deleteById(codigo);
+		
+		attributes.addFlashAttribute("mensagem", "Título excluído com sucesso!");
+		return "redirect:/titulos";
 	}
 	
 	@ModelAttribute("todosStatusTitulo")
